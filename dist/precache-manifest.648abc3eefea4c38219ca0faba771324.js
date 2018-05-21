46712d60333e1eacd3c0df32c8960f2fa474b055
tangf <tangf@yunyichina.cn>
2018-05-21 23:01:32 +0800

    构建

---
+++ dist/precache-manifest.648abc3eefea4c38219ca0faba771324.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors.347abf66920e51a70da0.js"
+  },
+  {
+    "url": "runtime.347abf66920e51a70da0.js"
+  },
+  {
+    "url": "main.347abf66920e51a70da0.js"
+  },
+  {
+    "revision": "74b8aeb0e9c75737a6c0e6e36fd1e10b",
+    "url": "index.html"
+  },
+  {
+    "revision": "62fbce59b1d8caf7051a",
+    "url": "2.css"
+  }
+];
